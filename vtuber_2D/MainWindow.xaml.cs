3c231ce8c65aa0c06e2886a6fe6ab12975e3488a
unknown <ihsanhassan.business@gmail.com>
2024-02-15 08:31:09 -0800

    update: added current file

---
+++ vtuber_2D/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using NAudio.Wave;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using vtuber_2D.manager;
+
+namespace vtuber_2D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AudioDeviceManager audioManger = new AudioDeviceManager();
+            cmbMicrophones.ItemsSource = audioManger.GetAudioDevices();
+        }
+
+
+        private void cmbMicrophones_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Debug.WriteLine("selection changed");
+            string selectedDevice = cmbMicrophones.SelectedItem as string;
+
+            if (selectedDevice != null)
+            {
+                // Check if the selected microphone is picking up sound
+                // You can use NAudio or other libraries to implement this functionality
+                // For example, you can use NAudio's WasapiLoopbackCapture to capture audio
+                // and check if the audio level is above a certain threshold.
+                // Refer to NAudio documentation for more details.
+                // Here's a simplified example using NAudio's WasapiLoopbackCapture:
+                Debug.WriteLine("not null");
+                using (var capture = new WasapiLoopbackCapture())
+                {
+                    capture.DataAvailable += (s, args) =>
+                    {
+                        // Check audio data here and update UI accordingly
+                        // For example, you can update a label or change the color of a button
+                        // based on the audio level.
+                        Debug.WriteLine("has audio");
+                    };
+
+                    capture.StartRecording();
+                }
+            }
+        }
+    }
+}
---
+++ vtuber_2D/manager/AudioDeviceManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+using System.Collections.ObjectModel;
+
+namespace vtuber_2D.manager
+{
+    public class AudioDeviceManager
+    {
+
+        public ObservableCollection<string> GetAudioDevices()
+        {
+            ObservableCollection<string> devices = new ObservableCollection<string>();
+            var enumerator = new MMDeviceEnumerator();
+            var devicesCollection = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+
+            foreach ( var device in devicesCollection)
+            {
+                devices.Add(device.FriendlyName);
+            }
+
+            return devices;
+        }
+    }
+}
